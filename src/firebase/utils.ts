08954cef6169772ee15dd111b9c69eaf713df184
lehuynhdong1 <53470576+lehuynhdong1@users.noreply.github.com>
2021-09-27 20:52:25 +0700

    Add redux saga user

--- src/firebase/utils.ts
+++ src/firebase/utils.ts
@@ -1,21 +1,11 @@
-import {
-  initializeApp
-} from 'firebase/app';
-import {
-  getAuth,
-  GoogleAuthProvider
-} from "firebase/auth";
-import {
-  getFirestore
-} from "firebase/firestore";
-import {
-  firebaseConfig
-} from './config';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { child, get, getDatabase, ref, set } from "firebase/database";
+import { firebaseConfig } from './config';
 
 initializeApp(firebaseConfig);
 
 export const auth = getAuth();
-export const firestore = getFirestore();
 
 export const GoogleProvider = new GoogleAuthProvider();
 GoogleProvider.setCustomParameters({
@@ -23,6 +13,36 @@ GoogleProvider.setCustomParameters({
 });
 
 export const handleUserProfile = async ({ userAuth, additionalData }: any) => {
+  if (!userAuth) {
+    return;
+  };
+  const { uid } = userAuth;
+  const dbRef = ref(getDatabase());
+  const userRef = child(dbRef, `users/${uid}`);
+  get(userRef).then(async (snapshot) => {
+    if (snapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const timestamp = new Date();
+      const userRoles = ['user'];
+
+      try {
+        const db = getDatabase();
+        await set(ref(db, 'users/' + uid), {
+          displayName,
+          email,
+          createdDate: timestamp,
+          userRoles,
+          ...additionalData
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      console.log("No data available");
+    }
+  });
+
+  return userRef;
 };
 
 export const getCurrentUser = () => {
---
+++ src/redux/User/user.sagas.ts
@@ -0,0 +1,140 @@
+import { DataSnapshot } from "@firebase/database";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword, signInWithPopup, signOut
+} from "firebase/auth";
+import {
+  all,
+  call,
+  put,
+  takeLatest
+} from 'redux-saga/effects';
+import {
+  auth,
+  getCurrentUser
+} from '../../firebase/utils';
+import { GoogleProvider, handleUserProfile } from './../../firebase/utils';
+import {
+  signInSuccess,
+  signOutUserSuccess,
+  userError
+} from './user.actions';
+import userTypes from './user.types';
+
+export function* getSnapshotFromUserAuth(user: any, additionalData = {}) {
+  try {
+    const userRef: unknown = yield call(handleUserProfile, { userAuth: user, additionalData });
+    const snapshot: DataSnapshot = yield userRef;
+    if (snapshot) {
+      yield put(signInSuccess({ snapshot }));
+    }
+
+  } catch (err) {
+    // console.log(err);
+  }
+}
+
+export function* emailSignIn({ payload: { email, password } }: any) {
+  try {
+    const {
+      user
+    } = yield signInWithEmailAndPassword(auth, email, password);
+    debugger;
+    yield getSnapshotFromUserAuth(user);
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* onEmailSignInStart() {
+  yield takeLatest(userTypes.EMAIL_SIGN_IN_START, emailSignIn);
+}
+
+export function* isUserAuthenticated() {
+  try {
+    const userAuth: unknown = yield getCurrentUser();
+    if (!userAuth) {
+      return
+    };
+    yield getSnapshotFromUserAuth(userAuth);
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* onCheckUserSession() {
+  yield takeLatest(userTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+export function* signOutUser() {
+  try {
+    yield signOut(auth);
+    yield put(
+      signOutUserSuccess()
+    )
+
+  } catch (err) {
+    // console.log(err);
+  }
+}
+
+export function* onSignOutUserStart() {
+  yield takeLatest(userTypes.SIGN_OUT_USER_START, signOutUser);
+}
+
+export function* signUpUser({ payload: { displayName, email, password, confirmPassword } }: any) {
+  if (password !== confirmPassword) {
+    const err = ['Password Don\'t match'];
+    yield put(
+      userError(err)
+    );
+    return;
+  }
+
+  try {
+    const {
+      user
+    } = yield createUserWithEmailAndPassword(auth, email, password);
+    const additionalData = {
+      displayName
+    };
+    yield getSnapshotFromUserAuth(user, additionalData);
+
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+export function* onSignUpUserStart() {
+  yield takeLatest(userTypes.SIGN_UP_USER_START, signUpUser);
+}
+
+export function* googleSignIn() {
+  try {
+    const {
+      user
+    } = yield signInWithPopup(auth, GoogleProvider);
+    yield getSnapshotFromUserAuth(user);
+
+  } catch (err) {
+    // console.log(err);
+  }
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(userTypes.GOOGLE_SIGN_IN_START, googleSignIn);
+}
+
+
+export default function* userSagas() {
+  yield all([
+    call(onEmailSignInStart),
+    call(onCheckUserSession),
+    call(onSignOutUserStart),
+    call(onSignUpUserStart),
+    call(onGoogleSignInStart),
+  ])
+}
